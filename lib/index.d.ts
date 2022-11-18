b3003562891001d0ab472b3d22b5c82c4a50554f
OpenSource <opensource@thearcadia.xyz>
2022-11-18 01:58:30 +0100

    Updated gitignore

--- lib/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { NextFunction, Request, Response } from "express";
-export declare const turnstile: {
-    validate: (secret: string) => (req: Request, res: Response, next: NextFunction) => Response<any, Record<string, any>> | undefined;
-};
